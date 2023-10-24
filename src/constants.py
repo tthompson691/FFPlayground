d009152c09713e444641808dab2be36687564962
tthompson691 <tthompson691@gmail.com>
2023-10-24 10:19:52 -0500

    add ruff, pre-commit hooks

--- src/constants.py
+++ src/constants.py
@@ -2,11 +2,13 @@ from league_secrets import LEAGUE_ID
 
 FANTASY_BASE_ENDPOINT_NEW = "https://fantasy.espn.com/apis/v3/games/ffl/leagueHistory/"
 
+
 def get_base_endpoint(year):
     if year > 2018:
         return f"https://fantasy.espn.com/apis/v3/games/ffl/seasons/{year}/segments/0/leagues/{LEAGUE_ID}"
     else:
-        return f"https://fantasy.espn.com/apis/v3/games/ffl/leagueHistory/{LEAGUE_ID}?seasonId={year}"
+        return (
+            f"https://fantasy.espn.com/apis/v3/games/ffl/leagueHistory/{LEAGUE_ID}?seasonId={year}"
+        )
 
     # return f"https://fantasy.espn.com/apis/v3/games/ffl/seasons/{year}/segments/0/leagues/{LEAGUE_ID}"
-   
